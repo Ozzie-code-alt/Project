39dc9c9129e7eda9dd0ad10ce524bc3b3c24ec13
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-30 23:20:39 +0800

    first commit

---
+++ projectType/src/App.tsx
@@ -0,0 +1,16 @@
+import TxtBox, {} from '../components/TxtBox'
+function App() {
+
+
+  return (
+    <div className="main-contianer align-items-center" >
+
+      <TxtBox />
+
+    </div>
+    
+    
+  )
+}
+
+export default App
